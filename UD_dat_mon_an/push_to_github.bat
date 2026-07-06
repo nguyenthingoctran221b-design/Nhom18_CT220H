11d9cdc0a4e79b932396c789b50cd7c272ade07a
Nguyen Thi Ngoc Tran <283592321+nguyenthingoctran221b-design@users.noreply.github.com>
2026-07-06 18:39:04 +0700

    Da hoan thien luong xu ly cua hoa don, bep, cap nhat dashboard danh thu, xuat bao cao, phan khu bep

--- UD_dat_mon_an/push_to_github.bat
+++ UD_dat_mon_an/push_to_github.bat
@@ -1,7 +1,7 @@
 @echo off
-echo Committing and pushing changes to GitHub...
+echo Committing and force pushing changes to GitHub...
 git add .
 git commit -m "Da hoan thien luong xu ly cua hoa don, bep, cap nhat dashboard danh thu, xuat bao cao, phan khu bep"
-git push
+git push -f
 echo Done!
 pause
