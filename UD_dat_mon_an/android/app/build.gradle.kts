a515a9f71300e0ee804f2ce7cedb70dcda6d4966
Nguyen Thi Ngoc Tran <283592321+nguyenthingoctran221b-design@users.noreply.github.com>
2026-08-09 23:03:34 +0700

    ci: Chuyen sang dung apksigner action ky release APK

--- UD_dat_mon_an/android/app/build.gradle.kts
+++ UD_dat_mon_an/android/app/build.gradle.kts
@@ -11,12 +11,6 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
-if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-}
-
 android {
     namespace = "com.nhom18.ud_dat_mon_an"
     compileSdk = flutter.compileSdkVersion
@@ -32,49 +26,17 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.nhom18.ud_dat_mon_an"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
-    signingConfigs {
-        create("release") {
-            storeType = "PKCS12"
-
-            val envKeyAlias = System.getenv("KEY_ALIAS")?.trim()
-            val envKeyPassword = System.getenv("KEY_PASSWORD")?.trim()
-            val envKeystorePath = System.getenv("KEYSTORE_PATH")?.trim()
-            val envKeystorePassword = System.getenv("KEYSTORE_PASSWORD")?.trim()
-
-            val sPass = if (!envKeystorePassword.isNullOrEmpty()) envKeystorePassword else keystoreProperties.getProperty("storePassword")
-            val alias = if (!envKeyAlias.isNullOrEmpty()) envKeyAlias else keystoreProperties.getProperty("keyAlias")
-
-            keyAlias = alias
-            storePassword = sPass
-            keyPassword = sPass
-
-            val keystorePath = if (!envKeystorePath.isNullOrEmpty()) envKeystorePath else keystoreProperties.getProperty("storeFile")
-            if (!keystorePath.isNullOrEmpty()) {
-                storeFile = file(keystorePath)
-            }
-
-            println("Release Signing Config -> Alias: '$keyAlias', StoreFile: '${storeFile?.name}'")
-        }
-    }
-
     buildTypes {
         release {
-            val releaseSigning = signingConfigs.getByName("release")
-            signingConfig = if (releaseSigning.storeFile != null && releaseSigning.storeFile!!.exists()) {
-                releaseSigning
-            } else {
-                signingConfigs.getByName("debug")
-            }
+            // Build với debug key mặc định, việc ký release APK sẽ do apksigner trong GitHub Actions đảm nhận
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
@@ -82,4 +44,3 @@ android {
 flutter {
     source = "../.."
 }
-
